fd8e6637351970d4b589faf8aacf1d0ddd28b9c0
MozammalHaq <mrmozammal@gmail.com>
2023-05-28 20:24:52 +0600

    Room details route sets

---
+++ src/components/Rooms/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from '../Heading/Heading';
+
+const Header = () => {
+    return (
+        <>
+            <Heading
+                title="Heading"
+                subtitle="Sub Title"
+                center={false}
+            ></Heading>
+        </>
+    );
+};
+
+export default Header;
